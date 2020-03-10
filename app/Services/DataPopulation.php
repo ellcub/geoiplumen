22e71c36b397cbe99d859bb8eeb1bc8ab86e7ebf
Ellen Cubbin <ellcub@gmail.com>
2020-03-10 14:50:29 +0000

    Download and unzip csv data

--- app/Services/DataPopulation.php
+++ app/Services/DataPopulation.php
@@ -4,10 +4,32 @@
 namespace App\Services;
 
 
+use Exception;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use ZipArchive;
 
 class DataPopulation
 {
+
+    /**
+     * Location to download data from
+     * @var string
+     */
+    protected $zipUrl = 'https://php-dev-task.s3-eu-west-1.amazonaws.com/GeoIPCountryCSV.zip';
+
+    /**
+     * Location to store zip file
+     * @var string
+     */
+    protected $storeZipFile = 'geoipcountrycsv.zip';
+
+    /**
+     * @var string
+     */
+    protected $storeCsv;
+
     /**
      * Check whether a table has any records
      *
@@ -17,5 +39,37 @@ class DataPopulation
     public function checkDataIsPopulated($table = 'geo_ip_country'): bool
     {
         return (bool)DB::table($table)->count();
+    }
+
+    /**
+     * Download data and store zip file
+     * Assume we want to download a fresh copy
+     */
+    public function downloadFile(): void
+    {
+        $client = new Client();
+        $response = $client->get($this->zipUrl);
+        Storage::put($this->storeZipFile, $response->getBody());
+    }
+
+    /**
+     * Extract csv file from the zip archive
+     * @throws \Exception
+     */
+    public function extractCSV()
+    {
+        $zipFile = storage_path('app/'.$this->storeZipFile);
+
+        $zip = new ZipArchive();
+        if ($zip->open($zipFile) === true) {
+            $zip->extractTo(storage_path('app/'));
+
+            // Get the filename of the csv, assuming one file is in the zip (index of 0)
+            $this->storeCsv = $zip->statIndex(0)['name'];
+
+            $zip->close();
+        } else {
+            throw new Exception('Failed to extract CSV');
+        }
     }
 }
